fed80f4b641b07d1ed37025d22bb911f481235ed
Sergey Engelke <s.engelke@promenergo-rt.ru>
2026-03-30 16:56:56 +0300

    plus is ready

--- CPP/main.cpp
+++ CPP/main.cpp
@@ -1,3 +1,5 @@
+#include <pthread.h>
+
 #include <fstream>
 #include <iostream>
 #include <map>
@@ -9,7 +11,8 @@ class Date {
   int GetDay() const { return day; };
   int GetMonth() const { return month; };
   int GetYear() const { return year; };
-  Date operator+(const int &n) const;
+  Date operator+(const int& n) const;
+  Date operator-(const int& n) const;
 
  private:
   int day, month, year;
@@ -46,7 +49,7 @@ void Date::setDefault() {
   year  = 1970;
 }
 
-Date Date::operator+(const int &n) const {
+Date Date::operator+(const int& n) const {
   int resultYear  = this->year;
   int resultMonth = this->month;
   int resultDay   = this->day;
@@ -55,34 +58,37 @@ Date Date::operator+(const int &n) const {
               (daysCountInYear + static_cast<int>(isLeap(resultYear))))) {
     daysAccum += daysCountInYear + static_cast<int>(isLeap(resultYear++));
   }
-  if (resultYear != this->year) {
-    resultMonth = 1;
-  }
   while (n / (daysAccum + (daysNumberInMonth.at(resultMonth)))) {
     daysAccum += daysNumberInMonth.at(resultMonth++);
+    if (resultMonth > 12) {
+      resultYear++;
+      resultMonth = 1;
+    }
   }
-  if (resultMonth != this->month) {
-    resultDay = 1 + n - daysAccum;
+  resultDay += n - daysAccum;
+  if (resultDay > daysNumberInMonth.at(resultMonth)) {
+    resultDay -= daysNumberInMonth.at(resultMonth++);
+  }
+  if (resultMonth > 12) {
+    resultYear++;
+    resultMonth = 1;
   }
   return Date(resultDay, resultMonth, resultYear);
 }
 
-std::ostream &operator<<(std::ostream &out, const Date &d) {
+std::ostream& operator<<(std::ostream& out, const Date& d) {
   out << d.GetDay() << "." << d.GetMonth() << "." << d.GetYear();
   return out;
 }
 
 int main() {
-  std::ifstream file("/home/sergey/Projects/Simple_C_CPP/Test/data");
+  std::ifstream file(
+      "/home/engelke-sergey/Projects/Simple_C_CPP/Test/input_data");
   int a, b, c;
   file >> a >> b >> c;
-  auto birthDay = Date(a, b, c);
+  auto dayFromFile = Date(a, b, c);
 
-  std::cout << birthDay + 1 << std::endl;
-  std::cout << birthDay + 366 << std::endl;
-  std::cout << birthDay + 367 << std::endl;
-  std::cout << birthDay + 500 << std::endl;
-  std::cout << birthDay + 900 << std::endl;
-
-  std::cout << birthDay + 9584 << std::endl;
+  std::cout << dayFromFile << std::endl;
+  std::cout << dayFromFile + 245 << std::endl;   // 15.02.2024
+  std::cout << dayFromFile + 1019 << std::endl;  // 30.03.2026
 }
