98c4d3db79abf35f8a4f1ae01c3e635e52e2507d
Sergey Engelke <s.engelke@promenergo-rt.ru>
2026-03-31 18:02:33 +0300

    Need to refactor

--- CPP/date.cpp
+++ CPP/date.cpp
@@ -42,7 +42,53 @@ Date Date::operator+(const int& n) const {
   return Date(resultDay, resultMonth, resultYear);
 }
 
+Date Date::operator-(const int& n) const {
+  // int resultYear  = this->year;
+  // int resultMonth = this->month;
+  // int resultDay   = this->day;
+  // int daysAccum   = 0;
+  // while (n / (daysAccum +
+  //             (daysCountInYear + static_cast<int>(isLeap(resultYear))))) {
+  //   daysAccum += daysCountInYear + static_cast<int>(isLeap(resultYear++));
+  // }
+  // while (n / (daysAccum + (daysNumberInMonth.at(resultMonth)))) {
+  //   daysAccum += daysNumberInMonth.at(resultMonth++);
+  //   if (resultMonth > 12) {
+  //     resultYear++;
+  //     resultMonth = 1;
+  //   }
+  // }
+  // resultDay += n - daysAccum;
+  // if (resultDay > daysNumberInMonth.at(resultMonth)) {
+  //   resultDay -= daysNumberInMonth.at(resultMonth++);
+  // }
+  // if (resultMonth > 12) {
+  //   resultYear++;
+  //   resultMonth = 1;
+  // }
+  // return Date(resultDay, resultMonth, resultYear);
+}
+
+int Date::operator-(const Date& date) const {
+  // return date.day;
+}
+
 std::ostream& operator<<(std::ostream& out, const Date& d) {
   out << d.GetDay() << "." << d.GetMonth() << "." << d.GetYear();
   return out;
 }
+
+const std::map<int, int> Date::daysNumberInMonth{
+      {1, 31},   // January
+      {2, 28},   // February
+      {3, 31},   // March
+      {4, 30},   // April
+      {5, 31},   // May
+      {6, 30},   // June
+      {7, 31},   // July
+      {8, 31},   // August
+      {9, 30},   // September
+      {10, 31},  // October
+      {11, 30},  // November
+      {12, 31}   // December
+  };
--- CPP/date.hpp
+++ CPP/date.hpp
@@ -11,24 +11,12 @@ class Date {
   int GetYear() const { return year; };
   Date operator+(const int& n) const;
   Date operator-(const int& n) const;
+  int operator-(const Date& date) const;
 
  private:
   int day, month, year;
   void setDefault();
-  const std::map<int, int> daysNumberInMonth{
-      {1, 31},   // January
-      {2, 28},   // February
-      {3, 31},   // March
-      {4, 30},   // April
-      {5, 31},   // May
-      {6, 30},   // June
-      {7, 31},   // July
-      {8, 31},   // August
-      {9, 30},   // September
-      {10, 31},  // October
-      {11, 30},  // November
-      {12, 31}   // December
-  };
+  static const std::map<int, int> daysNumberInMonth;
   static bool isLeap(int year) {
     return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
   };
